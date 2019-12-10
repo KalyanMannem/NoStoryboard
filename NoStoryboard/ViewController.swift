cc33540a1d2db3af7a1ff6d3380938cc318c1cf0
Kalyan <kmannem2@gmail.com>
2019-12-10 16:13:42 +0530

    Initial Commit

---
+++ NoStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoStoryboard
+//
+//  Created by Kalyan Mannem on 10/12/19.
+//  Copyright © 2019 CompIndia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
