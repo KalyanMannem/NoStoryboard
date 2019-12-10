1106cb7596525ded2fc2c3fe79c3810d9f0ca3c5
Kalyan <kmannem2@gmail.com>
2019-12-10 18:10:21 +0530

    Intial Project Setup

--- NoStoryboard/ViewController.swift
+++ NoStoryboard/ViewController.swift
@@ -8,13 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    let tableView = UITableView()
+    var safeArea: UILayoutGuide!
+    var characters = ["Link", "Zelda", "Ganondorf", "Midna"]
+    override func loadView()
+    {
+        super.loadView()
+        safeArea = view.layoutMarginsGuide
+        setUpTableView()
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+    }
+    
+    func setUpTableView()
+    {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
+}
 
+extension ViewController: UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return characters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = characters[indexPath.row]
+        return cell
+    }
 }
 
